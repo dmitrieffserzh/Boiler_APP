5922bbcc6fc9434027fa8746b61e36e43261b8f5
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-01-21 18:29:07 +0300

    init

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,22 @@
 @extends('main.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">@lang('auth.verify_email')</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            @lang('auth.verify_refresh')
-                        </div>
-                    @endif
+    <div class="col">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="h4">@lang('auth.verify_email')</h1>
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        @lang('auth.verify_refresh')
+                    </div>
+                @endif
 
-                    @lang('auth.verify_check_your_email')
-                    @lang('auth.verify_not_receive'), <a href="{{ route('verification.resend') }}">@lang('auth.verify_request_repeatedly')</a>.
-                </div>
+                @lang('auth.verify_check_your_email')
+                @lang('auth.verify_not_receive'), <a
+                        href="{{ route('verification.resend') }}">@lang('auth.verify_request_repeatedly')</a>.
             </div>
         </div>
     </div>
-</div>
+
 @endsection
