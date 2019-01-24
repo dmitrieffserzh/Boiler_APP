7b873846a7c41ae0d30744b83c05b17be91abc90
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-01-24 13:20:06 +0300

    init

--- resources/views/main/news/index.blade.php
+++ resources/views/main/news/index.blade.php
@@ -5,20 +5,23 @@
         <h1 class="h4">Пользователи</h1>
 
             @foreach($categories as $category)
-                <div class="row shadow">
-                    <img src="{{ 'images/'. $category->image }}" alt="" >
-
+                <div class="row border">
+                    <div class="col-md-4 py-3">
+                    <img src="{{ 'images/'. $category->image }}" alt="" width="100%">
+                    </div>
+                    <div class="col-md-8 py-3">
                 <a class="font-weight-bold" href="{{ route('news.url', implode("/", $category->category->ancestorsAndSelf($category->category_id)->pluck('slug')->all()).'/'.$category->slug) }}"> {{ $category->title }}</a><br>
-                <strong class="small">{{$category->category->title}}</strong>
-                <br>
-                <br>
+                <strong class="small font-weight-bold">{{$category->category->title}}</strong>
                 </div>
-                <br>
-                <br>
+                </div>
 
             @endforeach
 
 
         {{ $categories->links() }}
     </div>
+@endsection
+
+@section('sidebar')
+    SIDEBAR
 @endsection
