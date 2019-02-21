feda15c49d228a143e32d055afe1957a47e2eb99
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2019-02-21 22:23:50 +0300

    init

--- public/js/app.js
+++ public/js/app.js
@@ -35218,8 +35218,8 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\DEVEPOPMENT\Boiler_APP\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\DEVEPOPMENT\Boiler_APP\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/dmitrieff/Проекты/Boiler_APP/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/dmitrieff/Проекты/Boiler_APP/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
