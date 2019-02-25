acb84bf598278d0cf3174512dbb2fdb7d77109e7
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-02-25 13:48:33 +0300

    init

--- public/js/app.js
+++ public/js/app.js
@@ -35221,8 +35221,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/dmitrieff/Проекты/Boiler_APP/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/dmitrieff/Проекты/Boiler_APP/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\DEVEPOPMENT\Boiler_APP\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\DEVEPOPMENT\Boiler_APP\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/main/news/grid/_wide.blade.php
+++ resources/views/main/news/grid/_wide.blade.php
@@ -1,6 +1,6 @@
 @if(isset($categorys[$numb]))
 <div class="col-lg-12 my-2 wide">
-    <div class="col-lg-12" style="background: url('{{ '/images/'. $categorys[$numb]->image }}'); min-height: 360px;background-size: contain;">
+    <div class="col-lg-12" style="background: url('{{ '/images/'. $categorys[$numb]->image }}'); min-height: 360px;background-size: cover;">
         <div class="py-5">
             <a class="font-weight-bold text-dark" href="{{ route('news.url', implode("/", $categorys[$numb]->category->ancestorsAndSelf($categorys[$numb]->category_id)->pluck('slug')->all()).'/'.$categorys[$numb]->slug) }}"> {{ $categorys[$numb]->title }}</a><br>
             <a href="{{ route('news.url', implode("/", $categorys[$numb]->category->ancestorsAndSelf($categorys[$numb]->category_id)->pluck('slug')->all())) }}" class="small font-weight-bold">{{$categorys[$numb]->category->title}}</a>
