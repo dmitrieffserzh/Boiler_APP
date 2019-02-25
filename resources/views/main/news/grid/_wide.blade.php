1416dc363f0c6823957c3d33403d0b0bea25ddea
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-02-25 15:11:19 +0300

    init

--- resources/views/main/news/grid/_wide.blade.php
+++ resources/views/main/news/grid/_wide.blade.php
@@ -1,10 +1,16 @@
 @if(isset($categories[$numb]))
-<div class="col-lg-12 my-2 wide">
-    <div class="col-lg-12" style="background: url('{{ '/images/'. $categories[$numb]->image }}'); min-height: 360px;background-size: cover;">
-        <div class="py-5">
-            <a class="font-weight-bold text-dark" href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all()).'/'.$categories[$numb]->slug) }}"> {{ $categories[$numb]->title }}</a><br>
-            <a href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all())) }}" class="small font-weight-bold">{{$categories[$numb]->category->title}}</a>
+    <div class="col-lg-12">
+        <div class="news-tile news-tile--wide">
+            <img src="{{ '/images/'. $categories[$numb]->image }}" alt="{{ $categories[$numb]->title }}"
+                 class="news-tile__image">
+            <div class="news-tile__content">
+                <a class="font-weight-bold text-dark"
+                   href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all()).'/'.$categories[$numb]->slug) }}">
+                    {{ $categories[$numb]->title }}
+                </a>
+                <a href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all())) }}"
+                   class="small font-weight-bold">{{$categories[$numb]->category->title}}</a>
+            </div>
         </div>
     </div>
-</div>
 @endif
