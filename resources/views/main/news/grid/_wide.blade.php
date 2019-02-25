575041080f2ae1a60736f2b8232bbe5b1a352396
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-02-25 15:38:14 +0300

    init

--- resources/views/main/news/grid/_wide.blade.php
+++ resources/views/main/news/grid/_wide.blade.php
@@ -4,12 +4,17 @@
             <img src="{{ '/images/'. $categories[$numb]->image }}" alt="{{ $categories[$numb]->title }}"
                  class="news-tile__image">
             <div class="news-tile__content">
-                <a class="font-weight-bold text-dark"
+                <a class="news-tile__title"
                    href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all()).'/'.$categories[$numb]->slug) }}">
                     {{ $categories[$numb]->title }}
                 </a>
                 <a href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all())) }}"
                    class="small font-weight-bold">{{$categories[$numb]->category->title}}</a>
+                <div class="news-tile__author">
+                    <img src="{{ UsersHelper::get_avatar($categories[$numb]->owner->avatar ?? null) }}" alt="" width="50px" style="border-radius: 50%">
+
+                    <a class="font-weight-bold text-white" href="{{ route('user.profile', $categories[$numb]->owner->route ?? $categories[$numb]->owner->username) }}">{{ $categories[$numb]->owner->username ?? $categories[$numb]->owner->first_name }}</a></li>
+                </div>
             </div>
         </div>
     </div>
