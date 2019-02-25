d73c1e9c844ea0c75afdf8106991f79617ccf30d
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-02-25 17:01:47 +0300

    init

--- resources/views/main/news/grid/_wide.blade.php
+++ resources/views/main/news/grid/_wide.blade.php
@@ -12,13 +12,11 @@
 
                 <a href="{{ route('news.url', implode("/", $categories[$numb]->category->ancestorsAndSelf($categories[$numb]->category_id)->pluck('slug')->all())) }}"
                    class="news-tile__category-title">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="#2ecdff"
-                         stroke="currentColor" stroke-width="0" stroke-linecap="round" stroke-linejoin="round"
-                         class="feather feather-circle">
-                        <circle cx="12" cy="12" r="10"></circle>
-                    </svg>
                     {{$categories[$numb]->category->title}}</a>
 
+                    <div class="news-tile__description">
+                        {!! str_limit( $categories[$numb]->content, $limit = 150, $end = '...') !!}
+                    </div>
 
                 <div class="news-tile__author">
                     <img src="{{ UsersHelper::get_avatar($categories[$numb]->owner->avatar ?? null) }}" alt="{{ $categories[$numb]->owner->username ?? $categories[$numb]->owner->first_name }}"
