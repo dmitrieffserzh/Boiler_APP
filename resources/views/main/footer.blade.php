0ff39a6b2ea9fc562b11c41523187e43e4bdaef3
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-03-20 11:21:47 +0300

    init

--- resources/views/main/footer.blade.php
+++ resources/views/main/footer.blade.php
@@ -1,5 +1,53 @@
 <footer class="footer">
-    <div class="container">
-        FOOTER
+    <div class="footer__content">
+       <div>FOOTER</div>
+
+        <ul>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+        </ul>
+
+
+
+        <ul>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+        </ul>
+
+
+        <ul>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+        </ul>
+
+
+        <ul>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+            <li><a href="" class="link">Ссылка</a></li>
+        </ul>
+
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </div>
+    <div class="footer-bottom">
+        <div class="footer__content">
+            FOOTER BOTTOM
+        </div>
     </div>
 </footer>
